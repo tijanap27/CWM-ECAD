f7d891f62de8849ad6d0788b636bd12675f04f10
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-14 16:10:56 +0100

    Update top.v

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,29 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module AC(
+    input clk,
+    input [4:0] temperature,
+    output reg heating,
+    output reg cooling
+    );
+
+    always @(posedge clk) begin
+      if ((heating==1)&(cooling==0))
+        if(temperature>=5'b10100)
+          heating = 0
+      if ((heating==0)&(cooling==0)) 
+        if(temperature<=5'b10010)
+          heating = 1
+        if (temperature>=5'b10110)
+          cooling = 1
+      if((heating==0)&(cooling==1))
+        if (temperature<=5'b10100)  
+          cooling = 0
+     end
+
+endmodule
+

8058b2fb9bfd72888dd21e040d8c9e6352db4d42
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-14 14:45:29 +0100

    Update top.v

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Tijana Petrovic
+// Date: 14.06.2021.
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,29 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+    
+
+`timescale 1ns / 100ps
+
+module LED(
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0] colour
+    );
+
+    always @(posedge clk) begin
+		  if (rst)
+			  colour <= 3'b000;
+		  else begin
+        if ((colour == 3'b000) | (colour == 3'b111))
+            colour <= 3'b001;
+        if (colour < 3'b110)
+			    colour <= colour + button;
+		    else
+			    colour <= button ? 3'b001 : colour;
+		    end
+	   end
+
+endmodule
 

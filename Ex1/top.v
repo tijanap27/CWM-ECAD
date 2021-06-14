5452a937c108d352257335c644d1126adf079b0f
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-14 11:48:50 +0100

    Update top.v

--- Ex1/top.v
+++ Ex1/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #1 - Electric Roller Blinds
-// Student Name:
-// Date: 
+// Student Name: Tijana Petorvic
+// Date: 14.06.2021.
 //
 //  Description: In this exercise, you need to design a logical unit, where the  
 //  where the inputs a,b select which roller control signal func to output according to the following

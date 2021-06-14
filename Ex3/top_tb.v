4f45e2ee126878ea1d341314af0b5a79fbc2dcbc
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-14 13:06:07 +0100

    Update top_tb.v

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -23,14 +23,14 @@ module top_tb(
     wire [7:0] counter_out;
     reg [7:0] counter_out_prev;
 
-    initial begin
+    inital
+    begin
         clk = 1'b0;
     
         forever
             #(CLK_PERIOD/2) clk=~clk;
     end
 
-//Todo: User logic
     initial begin
 	    err = 0;
 	    change = 1;
@@ -39,38 +39,39 @@ module top_tb(
 	    on_off = 0;
 	    #6
 	    forever begin
-            #(CLK_PERIOD - 6) 
-		    if (counter_out != counter_out_prev)
-		    begin
-		        $display("***TEST FAILED! tick gap error");
-		        err = 1;
-		    end
-		    #6
-            if((on_off&&(counter_out==counter_out_prev))|(!on_off&&(counter_out!=counter_out_prev))) begin
+            	#(CLK_PERIOD - 6) 
+		if (counter_out != counter_out_prev)
+		begin
+			$display("***TEST FAILED! tick gap error");
+			err = 1;
+		end
+		#6
+            	if((on_off&&(counter_out==counter_out_prev))|(!on_off&&(counter_out!=counter_out_prev))) begin
 		        $display("***TEST FAILED! enable error");
 		        err = 1;
-		    end
-            if((change&&(counter_out!=counter_out_prev + 1))|(!change&&(counter_out!=counter_out_prev - 1))) begin
+		end
+            	if((change&&(counter_out!=counter_out_prev + 1))|(!change&&(counter_out!=counter_out_prev - 1))) begin
 		        $display("***TEST FAILED! direction error");
 		        err = 1; 
-		    end
-		    if(rst&&(counter_out!=0)) begin
+		end
+		if(rst&&(counter_out!=0)) begin
 		        $display("***TEST FAILED! rst error");
 		        err = 1;
-		    end
-	    counter_out_prev = counter_out;
-            if (on_off == 0)
-            on_off = 1;
-        if (counter_out==8'b00000011)
-            change = 0;
-            if ((change == 0) & (counter_out == 8'b00000001))
-		    rst = 1;
-	    if (counter_out == 0)
-		    rst = 0;
-	    if (rst == 1)
-		    change = 1;
-        end
-	end
+		end
+		    
+	    	counter_out_prev = counter_out;
+            	if (on_off == 0)
+            	on_off = 1;
+        	if (counter_out==8'b00000011)
+            		change = 0;
+            	if ((change == 0) & (counter_out == 8'b00000001))
+			rst = 1;
+	    	if (counter_out == 0)
+			rst = 0;
+	    	if (rst == 1)
+			change = 1;
+        	end
+		end
     
     initial begin
         #50 

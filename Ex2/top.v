ce4a9ced0b65703a1d4ba5e345dfd5b661f29c2b
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-14 11:59:06 +0100

    Update top.v

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 - Doorbell Chime
-// Student Name:
-// Date: 
+// Student Name: Tijana Petrovic
+// Date: 14.06.2021.
 //
 //  Description: In this exercise, you need to design a multiplexer that chooses between two sounds, where the  
 //  output is delayed by 5 ticks (not clocks!) and acts according to the following truth table:
@@ -21,12 +21,14 @@
 `timescale 1ns / 100ps
 
 module doorbell(
-    //Todo: define inputs here
+    input a,
+    input b, 
+    input sel,
+    output out
     );
     
-    //Todo: define registers and wires here
+    wire out;
 
-
-    //Todo: define your logic here                 
+    assign #5 out = (sel) ? b: a;
       
 endmodule

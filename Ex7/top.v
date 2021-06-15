80b2e4a397b3d75ac4fe51d172aaad7dfeb2ecaf
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-15 17:18:07 +0100

    Update top.v

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7 - Lights Selector
-// Student Name:
-// Date: 
+// Student Name: Tijana Petrovic
+// Date: 15.06.2021.
 //
 //  Description: In this exercise, you need to implement a selector between RGB 
 // lights and a white light, coded in RGB. If sel is 0, white light is used. If
@@ -13,5 +13,35 @@
 //  outputs:
 //           light [23:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module Lights_Selector(
+	input clk,
+	input sel,
+	input rst,
+	input button,
+	output [23:0] light
+	);
 
+	wire [2:0] colour;
+	wire [23:0] rgb;
+
+LED myled(.clk(clk),
+	        .rst(rst),
+	        .button(button),
+	        .colour(colour)
+	        );
+
+RGB_Converter myrgb(.clk(clk),
+	                  .colour(colour),
+	                  .enable(1'b1),
+	                  .rgb(rgb)
+	                  );
+
+doorbell mydoorbell(.a(24'hFFFFFF),
+	                  .b(rgb),
+	                  .sel(sel),
+	                  .out(light)
+	                   );
+
+endmodule

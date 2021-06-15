704c59a48594de0e45bef155401c96be14245431
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-15 14:20:08 +0100

    Update project.tcl

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -20,7 +20,13 @@ create_fileset -constrset -quiet constraints
 #add_files -fileset constraints -norecurse ${project_constraints}
 #set_property is_enabled false [get_files ${project_constraints}]
 
-#Todo: add verilog modules here
+#Code added for look up table
+#create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name ColourLookUp -verbose
+#set_property -dict [list CONFIG.Component_Name {ColourLookUp}] [get_ips ColourLookUp]
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name ColourLookUp2
+set_property -dict [list CONFIG.Component_Name {ColourLookUp2} CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/Documents/CWM-ECAD/Ex6/mem.coe}] [get_ips ColourLookUp2]
+reset_target all [get_ips ColourLookUp2] 
+
 read_verilog "top.v"
 read_verilog "top_tb.v"
 

a78ac18ee0b3f70cb3b68d593572bc0c10cb8c7a
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-15 15:42:16 +0100

    Update project.tcl

--- Ex6/project.tcl
+++ Ex6/project.tcl
@@ -20,12 +20,36 @@ create_fileset -constrset -quiet constraints
 #add_files -fileset constraints -norecurse ${project_constraints}
 #set_property is_enabled false [get_files ${project_constraints}]
 
-#Code added for look up table
-#create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name ColourLookUp -verbose
-#set_property -dict [list CONFIG.Component_Name {ColourLookUp}] [get_ips ColourLookUp]
-create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name ColourLookUp2
-set_property -dict [list CONFIG.Component_Name {ColourLookUp2} CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/Documents/CWM-ECAD/Ex6/mem.coe}] [get_ips ColourLookUp2]
-reset_target all [get_ips ColourLookUp2] 
+
+# IP Core
+#create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name block_memory_generator
+#set_property -dict [list CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24}] [get_ips block_memory_generator]
+# set_property -dict [list CONFIG.Component_Name {mybram}] [get_ips block_memory_generator]
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name block_memory_generator
+set_property -dict [list CONFIG.Component_Name {block_memory_generator} CONFIG.Write_Width_A {24} CONFIG.Write_Depth_A {8} CONFIG.Read_Width_A {24} CONFIG.Write_Width_B {24} CONFIG.Read_Width_B {24} CONFIG.Load_Init_File {true} CONFIG.Coe_File {/home/centos/CWM-ECAD/Ex6/mem.coe}] [get_ips block_memory_generator]
+reset_target all [get_ips block_memory_generator] 
+
+#Todo: add verilog modules here
+read_verilog "top.v"
+read_verilog "top_tb.v"
+
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
+set_property top ${sim_top} [get_filesets sim_1]
+set_property include_dirs ${proj_dir} [get_filesets sim_1]
+set_property simulator_language Mixed [current_project]
+set_property verilog_define { {SIMULATION=1} } [get_filesets sim_1]
+set_property -name xsim.more_options -value {-testplusarg TESTNAME=basic_test} -objects [get_filesets sim_1]
+set_property runtime {} [get_filesets sim_1]
+set_property target_simulator xsim [current_project]
+set_property compxlib.compiled_library_dir {} [current_project]
+set_property top_lib xil_defaultlib [get_filesets sim_1]
+update_compile_order -fileset sim_1
+
+launch_simulation
+run 1000ns
+
 
 read_verilog "top.v"
 read_verilog "top_tb.v"

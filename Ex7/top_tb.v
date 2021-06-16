555d8675b1694fff8c069198591ed474f02b1bdb
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-16 12:09:20 +0100

    Update top_tb.v

--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -20,7 +20,7 @@ module top_tb(
 	reg button;
 	reg err;
 	wire [23:0] light;
-  reg [23:0] prev_light;
+  	reg [23:0] prev_light;
 
 	initial begin
 	  clk = 1'b0;
@@ -28,52 +28,49 @@ module top_tb(
 		  #(CLK_PERIOD/2) clk=~clk;
 	end
   
-  
-	initial begin
-   	  rst = 1;
-	  button = 0;
-	  sel = 0;
-	  err = 0;
-    
-	#(CLK_PERIOD*3)
-	  if (light!=24'hFFFFFF) begin
-      		$display("*** TEST FAILED :( ***");
-		err = 1;
-	  end
-    
-  	  sel = 1;
-	  rst = 1;
-    
-	#(CLK_PERIOD*3)
-	if ((sel==1)&&(rst==1)&&(light!=24'b0)) begin
-      		$display("*** TEST FAILED :( ***");
-		err = 1;
-	end
-  
-    	sel = 1;
-	rst = 0;
-	button = 1;
-    	#CLK_PERIOD
-    	prev_light = light;
-    	#CLK_PERIOD
-    	if(prev_light!=light) begin
-      		$display("*** TEST FAILED! :( ***");
-	    	err = 1;
-	  end
-    
-    	button = 1;
-    	#CLK_PERIOD
-    	forever begin
-      		prev_light=light;
-		#(CLK_PERIOD*3)
-      		if(prev_light==light) begin
-        		$display("*** TEST FAILED! :( ***");
-	      		err = 1;
-		end
-    	end
- end
-  
-  
+initial begin
+    rst = 1;
+    button = 0;
+    sel = 0;
+    err = 0;
+
+    #(CLK_PERIOD)
+    if (light!=24'hFFFFFF) begin
+        $display("***TEST FAILED! :(***");
+        err = 1;
+    end
+
+    sel = 1;
+    prev_light = light;
+    rst = 0;
+    #(3*CLK_PERIOD)
+    if (light!=24'h0000FF) begin
+        $display("***TEST FAILED! :(***");
+        err=1;
+    end
+
+    #(CLK_PERIOD)
+    if (light!=prev_light) begin
+        $display("***TEST FAILED! :(***");
+        err=1; 
+    end
+
+    #(CLK_PERIOD)
+    forever begin
+        prev_light = light;
+        button = 1;
+
+        #(CLK_PERIOD)
+        button = 0;
+
+        #(CLK_PERIOD)
+        if (light == prev_light) begin
+            $display("***TEST FAILED! :(***");
+            err = 1;
+        end
+    end
+end
+	
 	initial begin
 	  #500
 	  if (err==0) begin

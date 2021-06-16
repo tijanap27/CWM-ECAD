f6f5eb7bd3ded0504a76793e9940cbbd1bc8f800
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-16 08:14:57 +0100

    Update top_tb.v

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -32,12 +32,12 @@ module top_tb(
     initial begin
     	clk = 0;
 	rst = 1;
-	button = 1;
     	err = 0;
 	#10
 	rst = 0;
   
     	forever begin
+		button = 1;
 		colour_prev = colour;
 		#10
 		if (rst&(colour!=0)) begin
@@ -63,37 +63,34 @@ module top_tb(
         		end
     		end
 	  	
+		button = 0;
+		colour_prev = colour;
+		#10
+		if (rst&(colour!=0)) begin
+        		$display("***TEST FAILED! Didn't reset! colour==%d, 	colour_prev==%d, rst='%d'***", colour, colour_prev, rst);
+        		err=1;
+      		end
+		if (((colour == 0)|(colour == 3'b111))&(rst!=0)) begin
+        		$display("***TEST FAILED! Out of bounds! colour==%d, colour_prev==%d, reset='%d' ***", colour, colour_prev, rst);
+	        	err = 1;
+		end
+		else begin
+        		if (colour_prev != 3'b110) begin
+          			if ((button&(colour!=colour_prev+1))| (!button&(colour!=colour_prev))) begin
+            				$display("***TEST FAILED! Button doesn't work! colour==%d, colour_prev==%d, button='%d' ***", colour, colour_prev, button);
+	          			err = 1;
+		     		end
+        		end
+			else begin
+				if ((button&(colour!=3'b001))| (!button&(colour!=colour_prev))) begin
+            				$display("***TEST FAILED! Button doesn't work for 6! colour==%d, colour_prev==%d, button='%d' ***", colour, colour_prev, button);
+	          			err = 1;
+		      		end
+        		end
+    		end		
 	end
 	
-	button = 0;
-	forever begin
-		colour_prev = colour;
-		#10
-		if (rst&(colour!=0)) begin
-        		$display("***TEST FAILED! Didn't reset! colour==%d, 	colour_prev==%d, rst='%d'***", colour, colour_prev, rst);
-        		err=1;
-      		end
-		if (((colour == 0)|(colour == 3'b111))&(rst!=0)) begin
-        		$display("***TEST FAILED! Out of bounds! colour==%d, colour_prev==%d, reset='%d' ***", colour, colour_prev, rst);
-	        	err = 1;
-		end
-		else begin
-        		if (colour_prev != 3'b110) begin
-          			if ((button&(colour!=colour_prev+1))| (!button&(colour!=colour_prev))) begin
-            				$display("***TEST FAILED! Button doesn't work! colour==%d, colour_prev==%d, button='%d' ***", colour, colour_prev, button);
-	          			err = 1;
-		     		end
-        		end
-			else begin
-				if ((button&(colour!=3'b001))| (!button&(colour!=colour_prev))) begin
-            				$display("***TEST FAILED! Button doesn't work for 6! colour==%d, colour_prev==%d, button='%d' ***", colour, colour_prev, button);
-	          			err = 1;
-		      		end
-        		end
-    		end
-	  	
-	end
-end
+	
 
   initial begin
     #300 

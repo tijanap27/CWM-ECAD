7ca8cc472aa0984dc824c722c88a911b96960a96
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-18 14:47:59 +0100

    Update project_sim.tcl

--- project_sim.tcl
+++ project_sim.tcl
@@ -19,6 +19,8 @@ create_fileset -constrset -quiet constraints
 
 read_verilog "top.v"
 read_verilog "AC.v"
+read_verilog "RGB_Converter.v"
+read_verilog "doorbell.v"
 read_verilog "lights.v"
 read_verilog "top_tb.v"
 

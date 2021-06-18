867afd5fe44e04e8e5f722b171ba8ea304d1942c
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-18 10:19:17 +0100

    Update top.v

--- top.v
+++ top.v
@@ -11,28 +11,36 @@ module Traffic(
   output yellow_p
    );
   
-  reg [1:0] state;
+  reg [2:0] state;
   
-  assign green_c = (state == 2'b00);
-  assign red_p = (state == 2'b00);
+  assign green_c = (state == 3'b000);
+  assign red_p = (state == 3'b000);
       
-  assign yellow_c = (state == 2'b01);
-  assign red_p = (state == 2'b01);
+  assign yellow_c = (state == 3'b001);
+  assign red_p = (state == 3'b001);
           
-  assign red_c = (state == 2'b10);
-  assign yellow_p = (state == 2'b10);
+  assign red_c = (state == 3'b010);
+  assign yellow_p = (state == 3'b010);
               
-  assign red_c = (state == 2'b11);
-  assign green_p = (state == 2'b11);
+  assign red_c = (state == 3'b011);
+  assign green_p = (state == 3'b011);
+	
+  assign red_c = (state == 3'b110);
+  assign yellow_p = (state == 3'b110);
+	
+  assign yellow_c = (state == 3'b101);
+  assign red_p = (state == 3'b101);
   
     
   always @(posedge clk) begin
     case (state)
-      2'b00 : state <= #1 button == 0 ? 2'b00 : 2'b01;
-      2'b01 : state <= #1 2'b10; 
-      2'b10 : state <= #1 2'b11;
-      2'b11 : state <= #1 2'b00;
-      default: state <= #1 2'b00;
+      3'b000 : state <= #1 button == 0 ? 3'b000 : 2'b001;
+      3'b001 : state <= #1 3'b010; 
+      3'b010 : state <= #1 3'b011;
+      3'b011 : state <= #1 3'b110;
+      3'b110 : state <= #1 3'b101;
+      3'b101 : state <= #1 3'b000;
+      default: state <= #1 3'b000;
     endcase
   end
 	       

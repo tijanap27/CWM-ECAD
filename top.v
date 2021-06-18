5a2ffdf53b309833e496c023b24a55f3c34ef4a2
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-18 12:48:04 +0100

    Update top.v

--- top.v
+++ top.v
@@ -4,16 +4,22 @@ module Smart_Home(
     input clk,
     input rst,
     input button,
-    input [4:0] temperature,
+    input temperature_0,
+    input temperature_1,
+    input temperature_2,
+    input temperature_3,
+    input temperature_4,
     output heating,
     output cooling,
-    output [2:0] colour
+    output colour_0,
+    output colour_1,
+    output colour_2
     );
   
   
   AC my_AC(
     .clk(clk),
-    .temperature(temperature),
+    .temperature({temperature_4,temperature_3,temperature_2,temperature_1,temperature_0}),
     .heating(heating),
     .cooling(cooling)
     );
@@ -23,7 +29,7 @@ module Smart_Home(
     .clk(clk),
     .rst(rst),
     .button(button),
-    .colour(colour)
+    .colour({colour_2, colour_1, colour_0})
     );
 
 endmodule

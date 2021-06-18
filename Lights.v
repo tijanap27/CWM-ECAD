ec2abf95919cb68859ea9bb22fed73c141a97515
tijanap27 <68991760+tijanap27@users.noreply.github.com>
2021-06-18 10:57:44 +0100

    Create Lights.v

---
+++ Lights.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 100ps
+
+module lights(
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0] colour
+    );
+
+    always @(posedge clk) begin
+		  if (rst)
+			  colour <= 3'b000;
+		  else begin
+        if ((colour == 3'b000) | (colour == 3'b111))
+            colour <= 3'b001;
+        if (colour < 3'b110)
+			    colour <= colour + button;
+		    else
+			    colour <= button ? 3'b001 : colour;
+		    end
+	   end
+
+endmodule
